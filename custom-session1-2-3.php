4772d67737e169a406847aaf50e5aea10194fa18
J-Pilecki <60199696+J-Pilecki@users.noreply.github.com>
2021-06-18 15:21:57 -0700

    Update custom-session1-2-3.php

--- custom-session1-2-3.php
+++ custom-session1-2-3.php
@@ -6,14 +6,6 @@
 
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 
-<!--
-
-Justin Pilecki
-
-May 3, 2020
-
--->
-
 <head>
     <title> Annual Raises </title>
     <meta charset="utf-8" />
@@ -26,11 +18,11 @@ May 3, 2020
         require_once("destroy_and_exit.php");
         require_once("create_login.php");
         require_once("create_empl_dropdown.php");
-        require_once("create_salary_form.php"); // <-----------hw10
-        require_once("update_salary.php"); // <-------------hw10
+        require_once("create_salary_form.php");
+        require_once("update_salary.php");
         require_once("get_empl_titles.php");
-        require_once("funcform.php"); // <-------------------hw11
-        require_once("funccall.php"); //<-------------hw11
+        require_once("funcform.php");
+        require_once("funccall.php");
     ?>
 
     <link href="http://users.humboldt.edu/smtuttle/styles/normalize.css"
@@ -41,7 +33,6 @@ May 3, 2020
 
 <body>
     <h1> Pet Care Boarding - Annual Raises! </h1>
-    <h2> Justin Pilecki CS 328 </h2>
  <p> <img src="https://www.nationaldebtrelief.com/wp-content/uploads/2015/10/The-Real-Effect-Of-A-Salary-Increase-On-Your-Life.jpg"
  alt="Hand pushing a search button for salary increase" />
  </p>
@@ -75,12 +66,10 @@ May 3, 2020
         $_SESSION["username"] = $username;
         $_SESSION["password"] = $password;
         create_empl_dropdown($username, $password);
-        $_SESSION["next_state"] = "empl_funct";   //    was empl_chosen
+        $_SESSION["next_state"] = "empl_funct";
     }
 
-// hw10 create salary form, update salary
-
-    elseif ($_SESSION['next_state'] == "empl_chosen")    //empl_chosen
+    elseif ($_SESSION['next_state'] == "empl_chosen")
     {
         $username = $_SESSION["username"];
         $password = $_SESSION["password"];
@@ -103,10 +92,8 @@ May 3, 2020
         $_SESSION["next_state"] = "salary_update";
     }
 
-// hw10
-
     elseif (($_SESSION['next_state'] == "is_user_done") &&
-            (array_key_exists("another_salary", $_POST)))  // was another_empl
+            (array_key_exists("another_salary", $_POST)))
     {
      	$username = $_SESSION["username"];
         $password = $_SESSION["password"];
@@ -130,12 +117,10 @@ May 3, 2020
     elseif ($_SESSION["next_state"] == "empl_chosen")
     {
      	get_empl_titles();
-        $_SESSION['next_state'] = "is_user_done";  //is_user_done
+        $_SESSION['next_state'] = "is_user_done";
     }
 
-// hw11 start
-
-   elseif ($_SESSION["next_state"] == "empl_funct")  //set status somewhere!
+   elseif ($_SESSION["next_state"] == "empl_funct")
    {
       $username = $_SESSION["username"];
       $password = $_SESSION["password"];
@@ -149,14 +134,11 @@ May 3, 2020
    {
       $username = $_SESSION["username"];
       $password = $_SESSION["password"];
-//	$salary = $_POST["salary"];
-//	$_SESSION["salary"] = $salary;
       $employee = $_SESSION["employee"];
       $response = $_POST["sub"];
       funccall($username, $password, $employee, $response);
       $_SESSION['next_state'] = "placeholder";
    }
-//hw11 end
 
     else
     {
