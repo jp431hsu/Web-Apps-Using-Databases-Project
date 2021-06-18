fbd6a6041936d940e064a218dc2334ad973e86e0
J-Pilecki <60199696+J-Pilecki@users.noreply.github.com>
2021-06-18 15:17:25 -0700

    Update hsu_conn_sess.php

--- hsu_conn_sess.php
+++ hsu_conn_sess.php
@@ -12,7 +12,7 @@
 	function: hsu_conn_sess: string string -> connection
         purpose: expects an Oracle username and password,
             and has the side-effect of trying to connect to
-            HSU's Oracle student database with the given
+            the database with the given
             username and password;
             returns the resulting connection object if
             successful,
@@ -33,9 +33,9 @@
 
         $db_conn_str =
             "(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)
-                                       (HOST = cedar.humboldt.edu)
-                                       (PORT = 1521))
-                            (CONNECT_DATA = (SID = STUDENT)))";
+                                       (HOST = /* Enter URL here */ )
+                                       (PORT = /* Enter port here */ ))
+                            (CONNECT_DATA = (SID = /* Enter SID here */ )))";
 
         // let's try to log on using this string!
 
